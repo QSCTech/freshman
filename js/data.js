fff6d3f10739a9cd0f61a4f53dc80fc76b5cd9b0
ALEJ WONG <alejwong@outlook.com>
2013-05-29 20:40:52 +0800

    Ch1入校更新

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ var data = [
       color: "#00c865",
       text: "C5-Text.png",
       title: "C5-Title.png",
-      img: "01-1338284703-2119010.png",
+      img: ["01-1338284703-2119010.png","asfalkjfh "]
       md: "example.md"
   }
 ];
